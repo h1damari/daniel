349f434cc65fa777404d69015be1c3e871942d76
hidamari <20251foz0020011@estudantes.ifpr.edu.br>
2026-08-20 11:30:13 -0300

    .

--- AULA 4 - Associação/Lista2Exercicio1.php/execucao.php
+++ AULA 4 - Associação/Lista2Exercicio1.php/execucao.php
@@ -3,27 +3,14 @@
 require_once('modelo/Carro.php');
 require_once('modelo/Fabricante.php');
 
+$fabricantes = array();
+
+$fabricantes["VW"] = new Fabricante("Volkswagen", "VW");  
+$fabricantes["GM"] = new Fabricante("Chevrolet", "GM");  
+$fabricantes["F"]  = new Fabricante("Fiat", "F");  
+$fabricantes["RN"] = new Fabricante("Renault", "RN");  
+
 $carros = array();
-
-$nome = "Volkswagen";
-$sigla = "VW";
-$volkswagen = new Fabricante($nome, $sigla);
-array_push($carros, $volkswagen);
-
-$nome = "Chevrolet";
-$sigla = "GM";
-$chevrolet = new Fabricante($nome, $sigla);
-array_push($carros, $chevrolet);
-
-$nome = "Fiat";
-$sigla = "F";
-$Fiat = new Fabricante($nome, $sigla);
-array_push($carros, $Fiat);
-
-$nome = "Renault";
-$sigla = "RN";
-$Renault = new Fabricante($nome, $sigla);
-array_push($carros, $Renault);
 
 do{
 
@@ -33,31 +20,45 @@ echo "3 - Listar Carro\n";
 echo "0 - Sair\n";
 $opcao = readline("escolha uma opção: ");
 
-switch($opcao){
-    case 1:
-        $modelo = readline("Modelo do carro: ");
-        $anoFabricacao = readline("Ano do carro: ");
-        $fabricante = readline("Fabricante: ");  
-        break;
-    
-    case 2:
-        $delete = readline("Qual o índice do carro a ser removido? ");
-        if(isset($carros[$delete]))
-            array_splice($carros, $delete);
-        else 
-            echo "Índice inválido!\n\n";
-        break;
-    
-    case 3:
-        foreach ($carros as $c) {
-            echo $c;
-        }
-        break;
+    switch($opcao){
+        case 1:
+            $modelo = readline("Modelo do carro: ");
+            $anoFabricacao = readline("Ano do carro: ");
+            $fabricanteSigla = readline("Sigla da fabricante (VW, GM, F, RN): ");
+
+            $fabricanteVerificada = null;
+            foreach($fabricantes as $f){
+                if($f ->getSigla() == $fabricanteSigla){
+                     $fabricanteVerificada = $f;
+                }
+            }
+
+            if($fabricanteVerificada != null){
+                $automovel = new Carro($modelo, $anoFabricacao, $fabricanteVerificada);
+                array_push($carros, $automovel);
+            } else{
+                echo "digita a porra de uma sigla valida\n\n";
+            }
+            break;
+        
+        case 2:
+            $delete = readline("Qual o índice do carro a ser removido? ");
+            if(isset($carros[$delete]))
+                array_splice($carros, $delete, 1);
+            else 
+                echo "Índice inválido!\n\n";
+            break;
+        
+        case 3:
+            foreach ($carros as $c) {
+                echo $c;
+            }
+            break;
 
 
-    case 0:
-        echo "\nSaindo...";
-        break;
-
+        case 0:
+            echo "\nSaindo...";
+            break;
+    }
 
 }while($opcao !=0);
