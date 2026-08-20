fdba9ae4e6d550a211d532503799c92cb33e5adc
hidamari <20251foz0020011@estudantes.ifpr.edu.br>
2026-08-20 13:22:27 -0300

    Update Carro.php

--- AULA 4 - Associação/Lista2Exercicio1.php/modelo/Carro.php
+++ AULA 4 - Associação/Lista2Exercicio1.php/modelo/Carro.php
@@ -12,10 +12,9 @@ class Carro{
         $this->modelo = $modelo; $this->anoFabricacao = $anoFabricacao; $this->fabricante = $fabricante;
     }
 
-    public function __construct($modelo, $anoFabricacao, Fabricante $fabricante) {
-        $this->modelo = $modelo;
-        $this->anoFabricacao = $anoFabricacao;
-        $this->fabricante = $fabricante;
+    public function __toString()
+    {
+        return "$this->modelo; $this->anoFabricacao; $this->fabricante;";
     }
 
     /**
